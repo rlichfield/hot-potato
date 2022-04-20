9f1dcc85f5a6dd8127cfec3fb04cf3b1dd677859
rlichfield <92386608+rlichfield@users.noreply.github.com>
2022-04-20 09:21:39 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,25 +2,16 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("3 2 1 Go!")
     timer = randint(4, 10)
     while (timer > 0) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
+        range = range.range(0, 4)
+        range.showColor(neopixel.colors(NeoPixelColors.Blue))
         basic.pause(500)
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
+        range = range.range(4, 7)
         basic.pause(500)
         timer += -1
+        range.showColor(neopixel.colors(NeoPixelColors.White))
     }
-    basic.showIcon(IconNames.Skull)
+    range.showColor(neopixel.colors(NeoPixelColors.Red))
 })
+let range: neopixel.Strip = null
 let timer = 0
-basic.showString("Hot Potato!")
+let strip = neopixel.create(DigitalPin.P0, 15, NeoPixelMode.RGB)
